8be448c8fc5ca32af6c140e8c60a238faa4a4d7f
Elam Daly <edaly@convene.com>
2022-01-22 20:45:17 -0500

    Add new image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
-      <Layout pageTitle='Home'>
-        <p>I'm making this by following the Gatsby Tutorial.</p>
-      </Layout>
+    <Layout pageTitle='Home'>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a pitbull"
+        src="../images/IMG_0433.jpeg" />
+    </Layout>
   )
 }
 
